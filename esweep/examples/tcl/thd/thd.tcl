aa044d6bf79ef48a917f9f9dfcdeb6f3316b907b
jfab <jfab>
2012-11-24 11:19:59 +0000

    The DFT was not correct because the harmonic frequencies were not correctly generated. This is now fixed. The recording subsystem is optimized to not start recording until the signal has properly settled.

--- esweep/examples/tcl/thd/thd.tcl
+++ esweep/examples/tcl/thd/thd.tcl
@@ -1,7 +1,7 @@
 load ../../../libesweeptcl.so esweep
 
 array set config {
-	Audio,Device 		audio:/dev/audio1
+	Audio,Device 		audio:/dev/audio0
 	Audio,Samplerate 	44100
 	Audio,Output,Channel 	1
 	Audio,Output,Max	0.9
@@ -9,10 +9,13 @@ array set config {
 	Audio,Input,Cal		0.29
 	Audio,Input,Cal,Base	2e-5
 
-	Measurement,F1		700
+	Measurement,F1		200
 	Measurement,F2		3000
 	Measurement,Step	3
 	Measurement,Harmonics	{2 3 5}
+	Measurement,IntraBurstDelay 200
+	Measurement,MaxWait	2000
+	Measurement,LvlAcc	0.1
 }
 
 # get options
@@ -33,26 +36,86 @@ proc openAudioDevice {device samplerate framesize} {
 	return $au
 }
 
-proc record {au sig_out sig_in sigsize} {
-	set maxFrames 20
+proc record {au sig_out sig_in sigsize useChan maxWait waitThres} {
 	set framesize [esweep::audioQuery -handle $au -param framesize]
 	set samplerate [esweep::audioQuery -handle $au -param samplerate]
 	set delay [expr {int(0.5+1000*$framesize/$samplerate)/4}]
 	set offset 0
 	set old_offset 0
+	set avgFrames 3
 
-	# fill input and output buffers
-	set frames 0
+	set in1 [esweep::create -type wave -size $framesize -samplerate $samplerate]
+	set in2 [esweep::create -type wave -size $framesize -samplerate $samplerate]
+	if {$useChan == 1} {
+		set useChan $in1
+	} else {
+		set useChan $in2
+	}
+	set in [list $in1 $in2]
 	esweep::audioSync -handle $au
-	while {$frames < $maxFrames} {
-		esweep::audioIn -handle $au -signals $sig_in -offset $old_offset
+	# fill input and output buffers, this usually takes 6 frames
+	# increase this when necessary, e. g. with WinMM output and its high delay
+	# 10 frames seem necessary
+	set frames 0
+	while {$frames < 6} {
+		esweep::audioIn -handle $au -signals $in
 		set offset [esweep::audioOut -handle $au -signals $sig_out -offset $old_offset]
-		if {$offset > $old_offset} {
+		# only count full frames
+		if {$offset - $old_offset >= $framesize} {
 			incr frames
 		}
 		# allow wrap around
 		if {$offset >= $sigsize} {set offset 0}
 		set old_offset $offset
+		# this short delay reduces CPU load
+		after $delay
+	}
+
+	# wait for signal to settle
+	# This is done by a calculating a running average over the last $avgFrames frames
+	# and comparing the current frame with it; if the level difference is below the threshold
+	# start recording.
+	
+	# Step 1: get $avgFrames frames
+	set frames 0
+	set lvlList [list]
+	set avg 0
+	while {$frames < $avgFrames} {
+		esweep::audioIn -handle $au -signals $in
+		set offset [esweep::audioOut -handle $au -signals $sig_out -offset $old_offset]
+		if {$offset - $old_offset >= $framesize} {
+			set lvl [esweep::sqsum -obj $useChan]
+			set avg [expr {$avg+$lvl}]
+			lappend lvlList $lvl
+			incr frames
+		}
+		if {$offset >= $sigsize} {set offset 0}
+		set old_offset $offset
+		after $delay
+	}
+
+	# Step 2: wait for signal to settle, but only $maxWait milliseconds (converted to frames)
+	set maxWait [expr {int(0.5+$maxWait*$samplerate/(1000.0*$framesize))}]
+	if {$maxWait < 2} {set maxWait 2}
+	# square the threshold because we are working with the square sum here
+	set waitThres [expr {$waitThres**2}]
+	set frames 0
+	while {$frames < $maxWait} {
+		esweep::audioIn -handle $au -signals $in
+		set offset [esweep::audioOut -handle $au -signals $sig_out -offset $old_offset]
+		if {$offset - $old_offset >= $framesize} {
+			set lvl [esweep::sqsum -obj $useChan]
+			set idx [expr {$frames % $avgFrames}]
+			set avg [expr {$avg-[lindex $lvlList $idx]+$lvl}]
+			lset lvlList $idx $lvl
+			incr frames
+		}
+		if {$offset >= $sigsize} {set offset 0}
+		set old_offset $offset
+		# check for threshold
+		if {abs((3*$lvl - $avg)/$avg) < $waitThres} {
+			break
+		}
 		after $delay
 	}
 
@@ -61,7 +124,7 @@ proc record {au sig_out sig_in sigsize} {
 	while {$frames > 0} {
 		esweep::audioIn -handle $au -signals $sig_in -offset $old_offset
 		set offset [esweep::audioOut -handle $au -signals $sig_out -offset $old_offset]
-		if {$offset > $old_offset} {
+		if {$offset - $old_offset >= $framesize} {
 			incr frames -1
 		}
 		# allow wrap around
@@ -70,6 +133,7 @@ proc record {au sig_out sig_in sigsize} {
 		after $delay
 	}
 	# make sure that we stop playback at the end of the output signal
+	# this reduces popping noises
 	if {$offset != 0} {
 		while {$offset < $sigsize} {
 			set offset [esweep::audioOut -handle $au -signals $sig_out -offset $offset]
@@ -112,15 +176,20 @@ proc measure {} {
 		set fftsize [expr {$fftsize < $framesize ? $framesize : $fftsize}]
 
 		set out [esweep::create -type wave -samplerate $config(Audio,Samplerate) -size $fftsize]
-		esweep::generate sine -obj out -frequency $f
+		set bin [esweep::generate sine -obj out -frequency $f]
 
 		foreach k [concat 1 $config(Measurement,Harmonics)] {
 			# generate sine and cosine for DFT
 			if {$k*$f >= $config(Audio,Samplerate)/2} continue
+			# esweep generates a sine that it fits with a integer number of periods
+			# in the supplied object. This may give not the desired harmonic here. By fiddling
+			# with the frequency parameter we need to make sure that it is always the correct 
+			# frequecy bin. 
+			set fd [expr {1.0*$k*$bin*$samplerate/$fftsize}]
 			set cos($k) [esweep::create -type wave -samplerate $config(Audio,Samplerate) -size $fftsize]
-			esweep::generate sine -obj cos($k) -frequency [expr {$k*$f}] -phase [expr {acos(0)}]
+			esweep::generate sine -obj cos($k) -frequency $fd -phase [expr {acos(0)}]
 			set sin($k) [esweep::create -type wave -samplerate $config(Audio,Samplerate) -size $fftsize]
-			esweep::generate sine -obj sin($k) -frequency [expr {$k*$f}] 
+			esweep::generate sine -obj sin($k) -frequency $fd
 		}
 		
 		# reduce output level to -3 dB peak
@@ -131,13 +200,15 @@ proc measure {} {
 		set in2 [esweep::create -type wave -samplerate $config(Audio,Samplerate) -size $fftsize]
 
 		# record signal
-		record $au [list $out $out] [list $in1 $in2] $fftsize
+		record $au [list $out $out] [list $in1 $in2] $fftsize $config(Audio,Input,Channel) \
+				$config(Measurement,MaxWait) $config(Measurement,LvlAcc)
 
 		if {$config(Audio,Input,Channel) == 1} {
 			set dut $in1
 		} else {
 			set dut $in2
 		}
+		set fft [esweep::clone -src $dut]
 
 		# do DFT for each harmonic
 		lappend result $f
@@ -158,6 +229,8 @@ proc measure {} {
 		lappend result $HD
 
 		set f [expr $f $step]
+		# intra burst delay
+		after $config(Measurement,IntraBurstDelay)
 	}
 	# close audio device
 	esweep::audioClose -handle $au
