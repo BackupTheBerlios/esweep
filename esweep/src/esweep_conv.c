23a9d2abab3b0b356a5e4cd860fdce2e620c942a
jfab <jfab>
2013-04-03 16:08:25 +0000

    added esweep_compress()

--- esweep/src/esweep_conv.c
+++ esweep/src/esweep_conv.c
@@ -14,12 +14,12 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* 
+/*
  * Type conversion
  */
 
-/* 
- * src/esweep_conv.c: 
+/*
+ * src/esweep_conv.c:
  * 11.01.2010, jfab: 	adaptation to the new data structures and types
  * 28.12.2010, jfab:	moved internal conversion functions to esweep_priv.c
  */
@@ -36,16 +36,16 @@ int esweep_toWave(esweep_object *obj) { /* TEST: OK */
 	Complex *cpx;
 	Wave *wave;
 	Polar *polar;
-	
-	ESWEEP_OBJ_ISVALID(obj, ERR_OBJ_NOT_VALID); 
+
+	ESWEEP_OBJ_ISVALID(obj, ERR_OBJ_NOT_VALID);
 	ESWEEP_ASSERT(obj->type != SURFACE, ERR_NOT_ON_THIS_TYPE);
-	
+
 	if (obj->size==0) { /* no need for conversion, simply change type */
-		obj->type=WAVE; 
-		return ERR_OK; 
+		obj->type=WAVE;
+		return ERR_OK;
 	}
 	if (obj->type==WAVE) { /* no need for conversion */
-		return ERR_OK; 
+		return ERR_OK;
 	}
 
 	ESWEEP_MALLOC(wave, obj->size, sizeof(Wave), ERR_MALLOC);
@@ -65,17 +65,17 @@ int esweep_toWave(esweep_object *obj) { /* TEST: OK */
 			obj->data=wave;
 			break;
 		default:
-			free(wave); 
+			free(wave);
 			ESWEEP_NOT_THIS_TYPE(obj->type, ERR_NOT_ON_THIS_TYPE);
 	}
-	
+
 	obj->type=WAVE;
-	
+
 	return ERR_OK;
 }
 
 
-/* convert a to type COMPLEX 
+/* convert a to type COMPLEX
 WAVE: the values are copied in the real part
 POLAR: calculate the standard Euler identity
 */
@@ -83,16 +83,16 @@ int esweep_toComplex(esweep_object *obj) { /* TEST: OK */
 	Complex *cpx;
 	Wave *wave;
 	Polar *polar;
-	
-	ESWEEP_OBJ_ISVALID(obj, ERR_OBJ_NOT_VALID); 
+
+	ESWEEP_OBJ_ISVALID(obj, ERR_OBJ_NOT_VALID);
 	ESWEEP_ASSERT(obj->type != SURFACE, ERR_NOT_ON_THIS_TYPE);
-	
+
 	if (obj->size==0) { /* no need for conversion, simply change type */
-		obj->type=COMPLEX; 
-		return ERR_OK; 
+		obj->type=COMPLEX;
+		return ERR_OK;
 	}
 	if (obj->type==COMPLEX) { /* no need for conversion */
-		return ERR_OK; 
+		return ERR_OK;
 	}
 
 	/* Convert data */
@@ -100,25 +100,25 @@ int esweep_toComplex(esweep_object *obj) { /* TEST: OK */
 		case WAVE:
 			ESWEEP_MALLOC(cpx, obj->size, sizeof(Complex), ERR_MALLOC);
 			wave=(Wave*) obj->data;
-			cpx=(Complex*) calloc(obj->size, sizeof(Complex)); 
+			cpx=(Complex*) calloc(obj->size, sizeof(Complex));
 			r2c(cpx, wave, obj->size);
 			free(wave);
 			obj->data=cpx;
 			break;
 		case POLAR:
-			/* Polar and Complex data types have the same internal shape, 
+			/* Polar and Complex data types have the same internal shape,
 			 * we can convert in-place */
 			polar=(Polar*) obj->data;
-			cpx=(Complex*) obj->data; 
+			cpx=(Complex*) obj->data;
 			p2c(cpx, polar, obj->size);
 			break;
 		default:
 			ESWEEP_NOT_THIS_TYPE(obj->type, ERR_NOT_ON_THIS_TYPE);
 			return ERR_NOT_ON_THIS_TYPE;
 	}
-	
+
 	obj->type=COMPLEX;
-	
+
 	return ERR_OK;
 }
 
@@ -129,34 +129,34 @@ COMPLEX: calculate the standard Euler identity
 int esweep_toPolar(esweep_object *obj) { /* TEST: OK */
 	Complex *cpx;
 	Polar *polar;
-	Wave *wave; 
-	
-	ESWEEP_OBJ_ISVALID(obj, ERR_OBJ_NOT_VALID); 
+	Wave *wave;
+
+	ESWEEP_OBJ_ISVALID(obj, ERR_OBJ_NOT_VALID);
 	ESWEEP_ASSERT(obj->type != SURFACE, ERR_NOT_ON_THIS_TYPE);
 
 	if (obj->size==0) { /* no need for conversion, simply change type */
-		obj->type=POLAR; 
-		return ERR_OK; 
+		obj->type=POLAR;
+		return ERR_OK;
 	}
 	if (obj->type==POLAR) { /* no need for conversion */
-		return ERR_OK; 
+		return ERR_OK;
 	}
 
 	switch (obj->type) {
 		case COMPLEX:
-			/* Polar and Complex data types have the same internal shape, 
+			/* Polar and Complex data types have the same internal shape,
 			 * we can convert in-place */
 			cpx=(Complex*) obj->data;
 			polar=(Polar*) obj->data;
-			c2p(polar, cpx, obj->size); 
+			c2p(polar, cpx, obj->size);
 			break;
 		case WAVE:
 			ESWEEP_MALLOC(polar, obj->size, sizeof(Complex), ERR_MALLOC);
-			wave=(Wave*) obj->data; 
+			wave=(Wave*) obj->data;
 			r2p(polar, wave, obj->size);
 			free(wave);
-			obj->data=polar; 
-			break; 
+			obj->data=polar;
+			break;
 		default:
 			ESWEEP_NOT_THIS_TYPE(obj->type, ERR_NOT_ON_THIS_TYPE);
 			return ERR_NOT_ON_THIS_TYPE;
@@ -169,32 +169,85 @@ int esweep_toPolar(esweep_object *obj) { /* TEST: OK */
 int esweep_switchRI(esweep_object *obj) { /* TEST: OK */
 	Complex *cpx;
 	Polar *polar;
-	Real tmp; 
-	int i; 
-	
-	ESWEEP_OBJ_ISVALID(obj, ERR_OBJ_NOT_VALID); 
-	ESWEEP_OBJ_NOTEMPTY(obj, ERR_EMPTY_OBJECT); 
-	
+	Real tmp;
+	int i;
+
+	ESWEEP_OBJ_ISVALID(obj, ERR_OBJ_NOT_VALID);
+	ESWEEP_OBJ_NOTEMPTY(obj, ERR_EMPTY_OBJECT);
+
 	/* Convert data */
 	switch (obj->type) {
 		case POLAR:
 			polar=(Polar*) obj->data;
-			for (i=0; i < obj->size; i++) polar[i].arg=M_PI_2-polar[i].arg; 
+			for (i=0; i < obj->size; i++) polar[i].arg=M_PI_2-polar[i].arg;
 			break;
-		case COMPLEX: 
-			cpx=(Complex*) obj->data; 
+		case COMPLEX:
+			cpx=(Complex*) obj->data;
 			for (i=0; i < obj->size; i++) {
-				tmp=cpx[i].real; 
-				cpx[i].real=cpx[i].imag; 
+				tmp=cpx[i].real;
+				cpx[i].real=cpx[i].imag;
 				cpx[i].imag=tmp;
 			}
-			break; 
+			break;
 		default:
 			ESWEEP_NOT_THIS_TYPE(obj->type, ERR_NOT_ON_THIS_TYPE);
 	}
-	
+
 	obj->type=COMPLEX;
-	
+
 	return ERR_OK;
 }
+
+int esweep_compress(esweep_object *obj, int32_t factor) {
+	Complex *cpx;
+	Polar *polar;
+  Wave *wave;
+	int i, j;
+
+	ESWEEP_OBJ_ISVALID(obj, ERR_OBJ_NOT_VALID);
+	ESWEEP_OBJ_NOTEMPTY(obj, ERR_EMPTY_OBJECT);
+  ESWEEP_ASSERT(factor > 0, ERR_BAD_PARAMETER);
+
+	switch (obj->type) {
+    case WAVE:
+      wave=(Wave*) obj->data;
+      for (i=0, j=0; j < obj->size; i++, j+=factor) { // compress data
+        wave[i]=wave[j];
+      }
+      for (; i < obj->size;i++) { // set all other to 0
+        wave[i]=0.0;
+      }
+      break;
+    case COMPLEX:
+      cpx=(Complex*) obj->data;
+      for (i=0, j=0; j < obj->size; i++, j+=factor) { // compress data
+        cpx[i].real=cpx[j].real;
+        cpx[i].imag=cpx[j].imag;
+      }
+      for (; i < obj->size;i++) { // set all other to 0
+        cpx[i].real=cpx[i].imag=0.0;
+      }
+      break;
+    case POLAR:
+      polar=(Polar*) obj->data;
+      for (i=0, j=0; j < obj->size/2-1; i++, j+=factor) { // compress data
+        polar[i].abs=polar[j].abs;
+        polar[i].arg=polar[j].arg;
+      }
+      for (; i < obj->size/2-1; i++) { // set all other to 0
+        polar[i].abs=polar[i].arg=0.0;
+      }
+      for (i=1; i < obj->size/2-1; i++) { /* restore hermitian redundancy */
+				polar[obj->size-i].abs=polar[i].abs;
+				polar[obj->size-i].arg=-polar[i].arg;
+      }
+      break;
+    default:
+			ESWEEP_NOT_THIS_TYPE(obj->type, ERR_NOT_ON_THIS_TYPE);
+  }
+
+  return ERR_OK;
+}
+
+
 
