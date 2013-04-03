ceb78ad861ebb9a99c332ed19b20c030fdb20bd3
jfab <jfab>
2013-04-03 15:24:36 +0000

    Typo

--- esweep/src/esweep_priv.c
+++ esweep/src/esweep_priv.c
@@ -46,15 +46,15 @@ int __esweep_assert(const char *file, int line, const char *assertion) {
 
 __EXTERN_FUNC__ void inline c2r(Real* output, Complex* input, int input_size) { /* complex 2 real */
   int i;
-  
+
   for (i=0;i<input_size;i++) {
-    output[i]=input[i].real; 
+    output[i]=input[i].real;
   }
 }
 
 __EXTERN_FUNC__ void inline r2c(Complex* output, Real* input, int input_size) { /* real 2 complex */
   int i;
-  
+
   for (i=0;i<input_size;i++) {
     output[i].real=input[i];
     output[i].imag=0.0;
@@ -63,15 +63,15 @@ __EXTERN_FUNC__ void inline r2c(Complex* output, Real* input, int input_size) { 
 
 __EXTERN_FUNC__ void inline p2r(Real* output, Polar* input, int input_size) { /* polar 2 real */
   int i;
-  
+
   for (i=0;i<input_size;i++) {
-    output[i]=input[i].abs*cos(input[i].arg); 
+    output[i]=input[i].abs*cos(input[i].arg);
   }
 }
 
 __EXTERN_FUNC__ void inline r2p(Polar* output, Real* input, int input_size) { /* real 2 polar */
   int i;
-  
+
   for (i=0;i<input_size;i++) {
     output[i].abs=input[i];
     output[i].arg=0.0;
@@ -80,19 +80,19 @@ __EXTERN_FUNC__ void inline r2p(Polar* output, Real* input, int input_size) { /*
 
 __EXTERN_FUNC__ void inline abs2r(Real* output, Polar* input, int input_size) { /* abs 2 real */
   int i;
-  
+
   for (i=0;i<input_size;i++) {
-    output[i]=input[i].abs; 
+    output[i]=input[i].abs;
   }
 }
 
 __EXTERN_FUNC__ void inline p2c(Complex *output, Polar *input, int input_size) { /* Polar 2 Complex */
   int i;
   Real abs, arg;
-  
+
   for (i=0;i<input_size;i++) {
     abs=input[i].abs;
-    arg=input[i].arg; 
+    arg=input[i].arg;
     output[i].real=abs*cos(arg);
     output[i].imag=abs*sin(arg);
   }
@@ -102,7 +102,7 @@ __EXTERN_FUNC__ void inline c2p(Polar *output, Complex *input, int input_size) {
   int i;
   Real real;
   Real imag;
-  
+
   for (i=0;i<input_size;i++) {
     real=input[i].real; /* this hack allows us to use a cast from Complex to Polar as output */
     imag=input[i].imag;
@@ -113,24 +113,24 @@ __EXTERN_FUNC__ void inline c2p(Polar *output, Complex *input, int input_size) {
 
 #ifdef REAL32
   #define MAXREAL FLT_MAX
-  #define -MAXREAL FLT_MIN
+  #define MINREAL FLT_MIN
 #else
   #define MAXREAL DBL_MAX
-  #define -MAXREAL DBL_MIN
+  #define MINREAL DBL_MIN
 #endif
 
 
 __EXTERN_FUNC__ int inline correctFpException(esweep_object* obj) {
-  int i; 
+  int i;
 
-  Wave *wave; 
-  Polar *polar; 
-  Complex *cpx; 
-  Surface *surf; 
+  Wave *wave;
+  Polar *polar;
+  Complex *cpx;
+  Surface *surf;
 
   if (fetestexcept(FE_INVALID)) {
     fprintf(stderr, "Invalid floating point operation occured.\n");
-    feclearexcept(FE_INVALID); 
+    feclearexcept(FE_INVALID);
     return 0;
   }
 
@@ -138,61 +138,61 @@ __EXTERN_FUNC__ int inline correctFpException(esweep_object* obj) {
     // find the errors and correct them by rounding to neares representable
     switch (obj->type) {
       case WAVE:
-        wave=(Wave*) obj->data; 
+        wave=(Wave*) obj->data;
         for (i=0; i < obj->size; i++) {
           if (isinf(wave[i])) {
             if (signbit(wave[i])) {
-              wave[i]=-MAXREAL; 
+              wave[i]=-MAXREAL;
             } else {
-              wave[i]=MAXREAL; 
+              wave[i]=MAXREAL;
             }
           }
         }
-        break; 
+        break;
       case POLAR:
-        polar=(Polar*) obj->data; 
+        polar=(Polar*) obj->data;
         for (i=0; i < obj->size; i++) {
           if (isinf(polar[i].abs)) {
             if (signbit(polar[i].abs)) {
-              polar[i].abs=-MAXREAL; 
+              polar[i].abs=-MAXREAL;
             } else {
-              polar[i].abs=MAXREAL; 
+              polar[i].abs=MAXREAL;
             }
           }
           if (isinf(polar[i].arg)) {
             if (signbit(polar[i].arg)) {
-              polar[i].arg=-MAXREAL; 
+              polar[i].arg=-MAXREAL;
             } else {
-              polar[i].arg=MAXREAL; 
+              polar[i].arg=MAXREAL;
             }
           }
         }
-        break; 
+        break;
       case COMPLEX:
-        cpx=(Complex*) obj->data; 
+        cpx=(Complex*) obj->data;
         for (i=0; i < obj->size; i++) {
           if (isinf(cpx[i].real)) {
             if (signbit(cpx[i].real)) {
-              cpx[i].real=-MAXREAL; 
+              cpx[i].real=-MAXREAL;
             } else {
-              cpx[i].real=MAXREAL; 
+              cpx[i].real=MAXREAL;
             }
           }
           if (isinf(cpx[i].imag)) {
             if (signbit(cpx[i].imag)) {
-              cpx[i].imag=-MAXREAL; 
+              cpx[i].imag=-MAXREAL;
             } else {
-              cpx[i].imag=MAXREAL; 
+              cpx[i].imag=MAXREAL;
             }
           }
         }
-        break; 
-      case SURFACE: 
-      default: 
-        break; 
+        break;
+      case SURFACE:
+      default:
+        break;
     }
-    feclearexcept(FE_OVERFLOW | FE_DIVBYZERO); 
+    feclearexcept(FE_OVERFLOW | FE_DIVBYZERO);
   }
-  return 1; 
+  return 1;
 }
 
